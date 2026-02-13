05f63fbc5419d88d304afeab4bf3e26f26ec7691
vmasdani <valianmasdani@gmail.com>
2026-02-13 10:54:05 +0700

    rpiconf

---
+++ start-kiosk.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+sleep 5
+
+chromium \
+    --user-data-dir=/home/gspe/.config/chromium-profile-1 \
+    --window-position=0,0 \
+    --new-window \
+    http://10.8.1.28:9001/#/productmonitoring &
+
+sleep 3
+
+chromium \
+    --user-data-dir=/home/gspe/.config/chromium-profile-2 \
+    --window-position=0,0 \
+    --new-window \
+    http://10.8.1.28:9001/#/productmonitoring &
