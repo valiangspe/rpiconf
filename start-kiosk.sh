c81551b1afccbca3de711d7dbed4b3c28b50c907
vmasdani <valianmasdani@gmail.com>
2026-02-13 11:19:06 +0700

    res

--- start-kiosk.sh
+++ start-kiosk.sh
@@ -11,6 +11,6 @@ sleep 3
 
 chromium \
     --user-data-dir=/home/rpi/.config/chromium-profile-2 \
-    --window-position=0,0 \
+    --window-position=1080,0 \
     --new-window \
     http://10.8.1.28:9001/#/productmonitoring &
