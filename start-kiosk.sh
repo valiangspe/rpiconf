39d44d7d521b855f41492fc2ecb23f5cba93332c
vmasdani <valianmasdani@gmail.com>
2026-02-13 11:15:55 +0700

    rpi

--- start-kiosk.sh
+++ start-kiosk.sh
@@ -2,7 +2,7 @@
 sleep 5
 
 chromium \
-    --user-data-dir=/home/gspe/.config/chromium-profile-1 \
+    --user-data-dir=/home/rpi/.config/chromium-profile-1 \
     --window-position=0,0 \
     --new-window \
     http://10.8.1.28:9001/#/productmonitoring &
@@ -10,7 +10,7 @@ chromium \
 sleep 3
 
 chromium \
-    --user-data-dir=/home/gspe/.config/chromium-profile-2 \
+    --user-data-dir=/home/rpi/.config/chromium-profile-2 \
     --window-position=0,0 \
     --new-window \
     http://10.8.1.28:9001/#/productmonitoring &
